76f15973e954c8441215c9f56d831db0ebe8d9bb
mikhail2013ru <mikhail2013ru@gmail.com>
2023-07-31 18:20:44 +0300

    кабздец

--- js/main.js
+++ js/main.js
@@ -2,31 +2,16 @@ const gloAcademyList = document.querySelector('.glo-academy-list')
 const trendingList = document.querySelector('.trending-list')
 const musicList = document.querySelector('.music-list')
 
-console.dir(trending)
-
 const createCard = (dataVideo) => {
     const imgUrl = dataVideo.snippet.thumbnails.high.url
     const videoId = dataVideo.id.videoId
     const titleVideo = dataVideo.snippet.title
     const dateVideo = dataVideo.snippet.publishedAt
     const channelTitle = dataVideo.snippet.channelTitle
-    let videoViews = document.querySelectorAll('.video-views')
-    let viewCount = ''
     
-    videoViews.forEach(elem => {
-        console.log(dataVideo.statistics)
-        if (dataVideo.statistics) {
-            viewCount = `${dataVideo.statistics.viewCount} views`
-            if (elem.textContent.trim() !== '') {
-                elem.style.marginRight = '5px'
-                elem.style.color = 'red'
-            }
-        }
-    })
-
     const card = document.createElement('div')
     card.classList.add('video-card')
-    card.innerHTML = `
+    card.innerHTML =`
             <div class="video-thumb">
                 <a class="link-video youtube-modal" href="https://youtu.be/${videoId}">
                 <img src="${imgUrl}" alt="" class="thumbnail">
@@ -35,22 +20,48 @@ const createCard = (dataVideo) => {
             <h3 class="video-title">${titleVideo}</h3>
             <div class="video-info">
                 <span class="video-counter">
-                <span class="video-views">${viewCount}</span>
+                <span class="video-views">${transfer()}</span>
                 <span class="video-date">${(new Date(dateVideo)).toLocaleString()}</span>
-                </span>
                 <span class="video-channel">${channelTitle}</span>
             </div>`
     return card
+}
+
+function transfer(send) {
+    console.log(send)
+    // return send
 }
 
 const createList = (wrapper, listVideo) => {
     wrapper.textContent = '';
 
     listVideo.forEach(item => {
-        const card = createCard(item)
+        let card = createCard(item)
+        viewCountCards(item)
         wrapper.append(card)
     });
+
+    function viewCountCards(dataVideo) {
+        let videoViews = document.querySelectorAll('.video-views')
+        let viewCount = ''
+        videoViews.forEach(elem => {
+            if (dataVideo.statistics) {
+                viewCount = `${dataVideo.statistics.viewCount} views`
+                // console.log(viewCount)
+                if (elem.textContent.trim() !== '') {
+                    elem.style.marginRight = '5px'
+                    elem.style.color = 'red'
+                }
+            }
+        })
+        
+        return transfer(viewCount)
+    }
 }
+
+
+
+
 
 createList(gloAcademyList, gloAcademy)
 createList(trendingList, trending)
---
+++ js/viewCountCards.js
@@ -0,0 +1,21 @@
+// const viewCountCards = (dataVideo) => {
+//     let videoViews = document.querySelectorAll('.video-views')
+//     let viewCount = ''
+//     videoViews.forEach(elem => {
+//         console.log(dataVideo)
+//         // if (dataVideo.statistics) {
+//         //     viewCount = `${dataVideo.statistics.viewCount} views`
+//         //     console.log(viewCount)
+//         //     if (elem.textContent.trim() !== '') {
+//         //         elem.style.marginRight = '5px'
+//         //         elem.style.color = 'red'
+//         //     }
+//         // }
+//     })
+
+//     return viewCount
+// }
+
+// const { viewCount } = viewCountCards()
+// export { viewCount }
+// export default viewCountCards
